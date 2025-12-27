b1c07910c9433dd11c1744055525376a99ea1dab
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2025-12-27 17:22:14 -0600

    Changed the addProduct() method signature to saveProduct() which updates or inserts a product through the repo. Autowired in the ProductRepo and updated all methods to use the repository so now it's working with a database

--- src/main/java/org/example/store_fullstack_backend/service/ProductService.java
+++ src/main/java/org/example/store_fullstack_backend/service/ProductService.java
@@ -1,9 +1,10 @@
 package org.example.store_fullstack_backend.service;
 
 import org.example.store_fullstack_backend.model.product.Product;
+import org.example.store_fullstack_backend.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,10 +12,7 @@ import java.util.List;
  */
 @Service
 public class ProductService {
-    /**
-     * {@code List<Product>} list of products handled by the service. see {@link org.example.store_fullstack_backend.model.product.Product}
-     */
-    private List<Product> products = new ArrayList<>();
+    ProductRepo repo;
 
     /**
      * Gets products with matching categories, returns all if no categories are provided
@@ -23,19 +21,24 @@ public class ProductService {
      */
     public List<Product> getProducts(List<String> categories) {
         if (categories == null || categories.isEmpty()) {
-            return products;
+            return repo.findAll();
         } else {
-            return products.stream()
+            return repo.findAll().stream()
                     .filter(prod -> prod.hasCategories(categories))
                     .toList();
         }
+    }
+
+    @Autowired
+    public void setRepo(ProductRepo repo) {
+        this.repo = repo;
     }
 
     /**
      * Adds a product to the list of managed products. see {@link org.example.store_fullstack_backend.model.product.Product}
      * @param product product to add
      */
-    public void addProduct(Product product) {
-        products.add(product);
+    public void saveProduct(Product product) {
+        repo.save(product);
     }
 }
