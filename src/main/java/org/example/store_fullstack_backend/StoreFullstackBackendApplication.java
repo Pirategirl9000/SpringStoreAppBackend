e5bfc5156805d04c6fe1bf753ba79405afa3e40f
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2025-12-27 13:49:46 -0600

    Added Javadocs

--- src/main/java/org/example/store_fullstack_backend/StoreFullstackBackendApplication.java
+++ src/main/java/org/example/store_fullstack_backend/StoreFullstackBackendApplication.java
@@ -3,8 +3,15 @@ package org.example.store_fullstack_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Driver class for the Spring App
+ */
 @SpringBootApplication
 public class StoreFullstackBackendApplication {
+	/**
+	 * Initializes the IoC container and begins the Spring application
+	 * @param args command line arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(StoreFullstackBackendApplication.class, args);
 	}
