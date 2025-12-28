2059d1fac142a07026104244f9859971f98a308d
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2025-12-27 18:03:46 -0600

    Added a query that grabs all unique categories from all Product entities

--- src/main/java/org/example/store_fullstack_backend/repository/ProductRepo.java
+++ src/main/java/org/example/store_fullstack_backend/repository/ProductRepo.java
@@ -2,8 +2,14 @@ package org.example.store_fullstack_backend.repository;
 
 import org.example.store_fullstack_backend.model.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
+
+    @Query("SELECT DISTINCT p.categories FROM Product p")
+    List<String> getDistinctCategories();
 }
